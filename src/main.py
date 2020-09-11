66dfccda14891d60f7b27416fc31bed6ef6342b3
koralowiec <arek0906@protonmail.com>
2020-09-11 09:19:41 +0200

    Image can be sent in base64

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import json
 import pytesseract
 import cv2
 import numpy as np
+import base64
 
 app = Flask(__name__)
 
@@ -14,14 +15,31 @@ def run_ocr(image, psm):
     number = pytesseract.image_to_string(image, config=tesseract_conf)
     print(f"Number (psm: {psm}): {number}")
 
+    return clean_up_number(number)
+
+
+def clean_up_number(number):
+    bad_chars = ["\n\x0c", "\x0c"]
+
+    for char in bad_chars:
+        number = number.replace(char, "")
+
     return number
 
 
 @app.route("/ocr", methods=["POST"])
 def ocr():
-    img = request.files["file"]
-    np_img = np.fromstring(img.read(), np.uint8)
-    img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+    is_image_in_base64 = request.args.get("base64", default=False)
+
+    if is_image_in_base64:
+        img = request.json["img"]
+        img_bytes = base64.b64decode(img)
+        np_img = np.frombuffer(img_bytes, dtype=np.uint8)
+    else:
+        img = request.files["file"]
+        np_img = np.fromstring(img.read(), np.uint8)
+
+    img = cv2.imdecode(np_img, cv2.IMREAD_UNCHANGED)
 
     numbers = []
     numbers.append(run_ocr(img, "9"))
